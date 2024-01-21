9c0462c27d7b1789714e760ef3c8abcd25159449
leo0901 <leozheng.inbox@gmail.com>
2024-01-20 22:35:05 -0800

    Fixed review page so that reviews updated properly

--- public/js/review.js
+++ public/js/review.js
@@ -3,7 +3,6 @@ document.getElementById("submit").addEventListener("click", e => submitRating())
 function fillCourseName() {
     db.collection("courses").doc(sessionStorage.getItem("course-id")).get()
         .then((doc) => {
-            console.log(doc.data());
             document.getElementById("course-placeholder").innerText = doc.data().name;
             document.getElementById("school-placeholder").innerText = doc.data().school;
         })
@@ -25,14 +24,28 @@ async function getName() {
 /* Creates a document into the reviews collection of the course with the user values. The name will not be saved
     if the anonymous box is checked */
 function submitRating() {
+
+    // Sets the value to one in case it is null.
+    if (document.getElementById("overall").value === "") {
+        document.getElementById("overall").value = 1;
+    }
+
+    if (document.getElementById("difficulty").value === "") {
+        document.getElementById("difficulty").value = 1;
+    }
+
+    if (document.getElementById("workload").value === "") {
+        document.getElementById("workload").value = 1;
+    }
+
+    var average = calculateAverage();
+
     if (document.getElementById("anonymous").checked) {
         /* Does not save the name. */
         var anonData = {
             id: localStorage.getItem("currentUid"),
             review: document.getElementById("review").value,
-            difficult: document.getElementById("difficulty").value,
-            workload: document.getElementById("workload").value,
-            overall: document.getElementById("overall").value,
+            stars: average,
             gradYear: document.getElementById("grad").value
         }
         db.collection("courses").doc(sessionStorage.getItem("course-id")).collection("reviews").add(anonData);
@@ -42,13 +55,25 @@ function submitRating() {
             user: localStorage.getItem("username"),
             id: localStorage.getItem("currentUid"),
             review: document.getElementById("review").value,
-            difficult: document.getElementById("difficulty").value,
-            workload: document.getElementById("workload").value,
-            overall: document.getElementById("overall").value,
+            stars: average,
             gradYear: document.getElementById("grad").value
         }
         db.collection("courses").doc(sessionStorage.getItem("course-id")).collection("reviews").add(data);
     }
+    db.collection("courses").doc(sessionStorage.getItem("course-id")).get()
+        .then((doc) => {
+            let newRating = (doc.data().totalRating + average);
+            let currentReviews = doc.data().numreviews;
+            currentReviews++;
+            return db.collection("courses").doc(sessionStorage.getItem("course-id")).update({
+                numreviews: currentReviews,
+                totalRating: newRating
+            })
+        })
+}
+
+function calculateAverage() {
+    return (parseFloat(document.getElementById("workload").value) + parseFloat(document.getElementById("overall").value) + parseFloat(document.getElementById("difficulty").value)) / 3;
 }
 
 fillCourseName();

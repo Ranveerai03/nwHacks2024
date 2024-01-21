4ec8efe5e700f964efc5ad5d58e770fadb866546
Ryan Lok <rlok.pc@gmail.com>
2024-01-20 18:57:33 -0800

    Finished logic for search page

--- public/js/search_page.js
+++ public/js/search_page.js
@@ -1,36 +1,52 @@
+const inputElement = document.querySelector("#search")
 
+inputElement.addEventListener('keyup', e => {
+    renderCourses(e.target.value);
+})
 
-function renderCourses(coursesArray) {
+function renderCourses(searchValue) {
+    const courseCollection = db.collection("courses")
+    const cardsContainer = document.querySelector("#cards-container")
+    cardsContainer.replaceChildren()
+    courseCollection.get().then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+            console.log(doc.data())
+            if (doc.data().name.toLowerCase().includes(searchValue.toLowerCase())) {
+                renderCard(doc.data())
+            }
+        })
+    })
+}
+
+function renderCard(element) {
     const template = document.querySelector("template")
     const cardsContainer = document.querySelector("#cards-container")
 
-    coursesArray.forEach(element => {
-        const card = template.content.cloneNode(true)
-        console.log(card)
-        const courseName = card.querySelector(".course-name")
-        courseName.innerText = element.name
-        
-        
-        const schoolName = card.querySelector(".institution-name")
-        schoolName.innerText = element.school
+    const card = template.content.cloneNode(true)
+    console.log(card)
+    const courseName = card.querySelector(".course-name")
+    courseName.innerText = element.name
 
-        const difficultyRating = card.querySelector(".difficulty-rating")
-        difficultyRating.innerText = calculateRating(element.totalDifficulty, element.numreviews).toString()
+    const schoolName = card.querySelector(".institution-name")
+    schoolName.innerText = element.school
 
-        const workloadRating = card.querySelector(".workload-rating")
-        workloadRating.innerText = calculateRating(element.totalWorkload, element.numreviews).toString()
+    const rating = card.querySelector(".rating")
+    rating.innerText = calculateRating(element)
 
-        const programRating = card.querySelector(".program-rating")
-        programRating.innerText = calculateRating(element.totalProgramRating, element.numreviews).toString()
-        
-        cardsContainer.appendChild(card)
-    });
+    const textContents = card.querySelector(".course-contents")
+    textContents.innerText = element.description
+
+    cardsContainer.appendChild(card)
 }
 
-function calculateRating(total, reviews) {
-    if (reviews == 0) {
+function calculateRating(element) {
+    console.log(element)
+    if (element.numreviews === 0 || typeof(element.numreviews) != 'number') {
         return "No Reviews"
     } else {
-        return total / reviews
+        
+
+        return element.totalRating / element.numreviews
     }
 }
+
